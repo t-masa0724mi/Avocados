b42767968295296a50463453a94af0b4f7c08cf1
masa950724mi <s100.iizuka.masami@gmail.com>
2023-06-27 14:26:45 +0900

    Initial Commit

---
+++ Avocados/AvocadosApp.swift
@@ -0,0 +1,17 @@
+//
+//  AvocadosApp.swift
+//  Avocados
+//
+//  Created by 飯塚政美 on 2023/06/27.
+//
+
+import SwiftUI
+
+@main
+struct AvocadosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
